b19b7a014b3921d5b3e60b83a0b035a6be568543
akrudra <abhishek.rudra02@gmail.com>
2021-02-23 14:33:23 +0530

    publishing Date

--- src/main/java/com/strandls/document/service/Impl/DocumentHelper.java
+++ src/main/java/com/strandls/document/service/Impl/DocumentHelper.java
@@ -334,7 +334,7 @@ public class DocumentHelper {
 
 			document = new Document(null, 0L, true, attribution, authorId, contributors, null, new Date(), notes, doi,
 					new Date(), Long.parseLong(fieldMapping.get("licenseId").toString()), null, null, title, type,
-					(ufile != null ? ufile.getId() : null), new Date(), null, null, null, null, null, null, null, null,
+					(ufile != null ? ufile.getId() : null), null, null, null, null, null, null, null, null, null,
 					new Date(), null, 0, 0, 205L, null, null, null, null, null, null, null, null, null, 1,
 					(rating != null) ? Integer.parseInt(rating) : 0, false, null, null, authors, journal, bookTitle,
 					year, month, volume, number, pages, publisher, school, edition, series, address, chapter, note,
